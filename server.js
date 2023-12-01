466cfe188d69357c843deb8efa6fc503ffcd7c07
rajat payal <rajatpayal2000@gmail.com>
2023-12-02 00:45:58 +0530

    add rules for draw and checkmate and check if king is in check

--- server.js
+++ server.js
@@ -10,22 +10,26 @@ const { v4: uuidv4 } = require('uuid');
 
 
 app.use(cors({
-  origin: 'http://localhost:3000' 
+  origin: '*' 
 }));
 
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:3000', 
+    origin: '*', 
     methods: ['GET', 'POST']
   }
 });
 app.use(express.json());
 
 let userMap = {};
-
 let players = [];
-
 let activeGames = {};
+
+
+// app.post('/',(req,res)){
+//   res.send()
+// }
+
 
 
 io.on('connection', (socket) => {
@@ -38,11 +42,12 @@ io.on('connection', (socket) => {
 
   socket.on('addPlayer', ({userId,name}) => {
 
-    console.log('now owreinds');
+    console.log('now friends');
     const existingPlayer = players.find(player => player.name === name);
         if (!existingPlayer) {
             const newPlayer = { id: userId, name };
             players.push(newPlayer);
+            console.log({players});
             userMap[newPlayer.id] = socket.id;
             io.emit('updatePlayers', players);
         }       
@@ -65,9 +70,22 @@ io.on('connection', (socket) => {
         white: invitingPlayerSocketId, // Inviter plays as white
         black: socket.id // Invitee plays as black
       };
-     
+
+      const inviterIndex = players.findIndex(x=> x.id == invitingPlayerId);
+      console.log(inviterIndex);
+      const inviterName = players[inviterIndex].name;
+
+      // const inviteeIdKey = userMap.keys(userMap).find(key=> userMap[key]== socket.id);
+      // const inviteeIndex = players.findIndex(x=> x.id == inviteeIdKey);
+      // const inviteeName = players[inviteeIndex].name;
+
+
+      const playerNames = {
+        white : inviterName,
+        black : "inviteeName"
+      }
       
-      io.to(gameId).emit('startGame', { gameId, playerRoles });
+      io.to(gameId).emit('startGame', { gameId, playerRoles,playerNames });
       // io.to(userMap[invitingPlayerId]).emit('joinGameRoom',gameId);
       const chess = new Chess();
       activeGames[gameId]=chess;
@@ -96,13 +114,15 @@ io.on('connection', (socket) => {
   // });
 
   
-
   socket.on('move', ({ gameId, move }) => {
     console.log(move);
     console.log(gameId);
     const chessGame = activeGames[gameId];
     if (chessGame && chessGame.move(move)) {
-      io.to(gameId).emit('gameUpdate', chessGame.fen());
+      const isInCheck = chessGame.inCheck();
+      const isCheckmate = chessGame.isCheckmate();
+      const isDraw = chessGame.isDraw() || chessGame.isStalemate() || chessGame.isThreefoldRepetition()||chessGame.isInsufficientMaterial();
+      io.to(gameId).emit('gameUpdate', { fen: chessGame.fen(), isInCheck ,isCheckmate,isDraw});
     } else {
       
       socket.emit('invalidMove');
